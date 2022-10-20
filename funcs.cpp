c4406fbb1dd0ffc835408d0bee6cd7be75eb3085
Shenghao <shenghaodong18@gmail.com>
2022-10-20 12:09:29 -0400

    done with part b need to add doctest and makefile

--- funcs.cpp
+++ funcs.cpp
@@ -10,3 +10,13 @@ std::string removeLeadingSpaces(std::string line){
     }
     return "";
 }
+
+int countChar(std::string line, char c){
+    int count = 0;
+    for(int i = 0; i < line.length(); i++){
+        if(line[i] == c){
+            count++;
+        }
+    }
+    return count;
+}
--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@
 
 int main(){
     //std::cout << removeLeadingSpaces("          Hello World!");
-    //./a.out < README.org
+    //./a.out < bad-code.cpp
     std::string badCode;
+    int bracketCounter = 0;
     while(std::getline(std::cin, badCode)){
+        std::string checkLine = removeLeadingSpaces(badCode);
+        if(checkLine.substr(0, 1) == "}"){
+            bracketCounter--;
+        }
+        for(int i = 0; i < bracketCounter; i++){
+            std::cout << "\t";
+        }
         std::cout << removeLeadingSpaces(badCode) << std::endl;
+        bracketCounter += countChar(badCode, '{');
+        bracketCounter -= countChar(badCode, '}');
+        
     }
 
     return 0;

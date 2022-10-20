daabd26404c66d7d77b30ceea5fa5da9234c36c6
Shenghao <shenghaodong18@gmail.com>
2022-10-20 10:12:14 -0400

    fixed function and added printing for unindenting

--- funcs.cpp
+++ funcs.cpp
@@ -2,7 +2,7 @@
 
 std::string removeLeadingSpaces(std::string line){
     //removes spaces in front of a string
-    for(int i = 0; i < line.length() - 1; i++){
+    for(int i = 0; i < line.length(); i++){
         if(line.substr(i, 1) != " "){
             return line.substr(i);
             break;
--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "funcs.h"
 
 int main(){
-    std::cout << removeLeadingSpaces("          Hello World!");
-
+    //std::cout << removeLeadingSpaces("          Hello World!");
+    //./a.out < README.org
+    std::string badCode;
+    while(std::getline(std::cin, badCode)){
+        std::cout << removeLeadingSpaces(badCode) << std::endl;
+    }
 
     return 0;
 }
